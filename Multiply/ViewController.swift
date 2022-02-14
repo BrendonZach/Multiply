717e1dd2f5d6ecd47af1b856717bdb2a4e670895
Brendon Zach <brendon_zach@outlook.com>
2022-02-14 14:17:37 -0700

    began MVP

--- Multiply/ViewController.swift
+++ Multiply/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var number1: UITextField!
+    
+    @IBOutlet weak var number2: UITextField!
+    
+    @IBOutlet weak var answer: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateButton(_ sender: Any)
+    {
+        //The ?? is an optional. It protects our data from user error. It will assign a default value that you provide at the right of the ??
+        var firstNumber = number1.text ?? ""
+        var secondNumber = number2.text ?? ""
+        var number1int = Int(firstNumber) ?? 0
+        var number2int = Int(secondNumber) ?? 0
+        var multiplication = number1int * number2int
+        answer.text = "\(multiplication)"
+        
+    
+        
+        
+    }
+    
 }
 
