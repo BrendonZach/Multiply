808eab5e46604afe0b5cc1b587e721e844e34666
Brendon Zach <brendon_zach@outlook.com>
2022-02-15 14:13:34 -0700

    did stretch one and two

--- Multiply/ViewController.swift
+++ Multiply/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var answer: UILabel!
     
+    @IBOutlet weak var Funky: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -24,10 +27,20 @@ class ViewController: UIViewController {
         //The ?? is an optional. It protects our data from user error. It will assign a default value that you provide at the right of the ??
         var firstNumber = number1.text ?? ""
         var secondNumber = number2.text ?? ""
+        Funky.isHidden = true
         var number1int = Int(firstNumber) ?? 0
         var number2int = Int(secondNumber) ?? 0
         var multiplication = number1int * number2int
         answer.text = "\(multiplication)"
+        //This is the first stretch witch dismisses the keyboard using the resignFirstResponder on the text fields.
+        number1.resignFirstResponder()
+        number2.resignFirstResponder()
+        if multiplication == 64
+        {
+            Funky.isHidden = false
+                        
+            
+        }
         
     
         
